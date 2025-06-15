e7283f3ede40cd2ccc3242b080e9c4ade561ea43
wayne <waynekim07@gmail.com>
2025-06-15 14:22:41 +0300

    fixes

--- src/Components/Todo/Todo.jsx
+++ src/Components/Todo/Todo.jsx
@@ -3,7 +3,7 @@ import { HiSaveAs } from "react-icons/hi";
 import { MdOutlineCancel,MdDelete } from "react-icons/md";
 import { FaEdit } from "react-icons/fa";
 import { RxCross1 } from "react-icons/rx";
-import { TodoContext } from '../App';
+import { TodoContext } from '../../App';
 
 function Todo({paras}) {
   const taskInput = useRef(null)
